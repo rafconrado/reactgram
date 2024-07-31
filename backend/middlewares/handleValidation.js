6c25448f32dd857fea7a1619b41b2afcc26d0a02
Rafael Conrado de Souza <rafconradoo@gmail.com>
2024-07-31 16:46:32 -0300

    att

--- backend/middlewares/handleValidation.js
+++ backend/middlewares/handleValidation.js
@@ -1,14 +1,19 @@
-const {validationResult} = require("express-validator")
+const { validationResult } = require("express-validator");
 
 const validate = (req, res, next) => {
+  const errors = validationResult(req);
 
-    const errors = validationResult(req)
+  if (errors.isEmpty()) {
+    return next();
+  }
 
-    if(errors.isEmpty()) {
-        return next()
-    }
+  const extractedErros = [];
 
-    const extractedErros = []
+  errors.array().map((err) => extractedErros.push(err.msg));
 
-    errors.array().map((err) => extractedErros.push(err.msg))
-}
+  return res.status(422).json({
+    errors: extractedErros,
+  });
+};
+
+module.exports = validate;
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -4,7 +4,11 @@ const router = express.Router();
 //controler
 const { register } = require("../controlles/userController");
 
+//middlewares
+const validate = require("../middlewares/handleValidation");
+const {userCreateValidation} = require("../middlewares/userValidation")
+
 //routes
-router.post("/register", register);
+router.post("/register", userCreateValidation(), validate, register);
 
 module.exports = router;
