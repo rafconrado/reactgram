41ab8e4bee0b42fc9182044d18eed8b6c2f80cae
Rafael Conrado de Souza <rafconradoo@gmail.com>
2024-08-08 17:57:40 -0300

    Update userController.js

--- backend/controlles/userController.js
+++ backend/controlles/userController.js
@@ -14,16 +14,40 @@ const generateToken = (id) => {
 
 //register user and sign in
 const register = async (req, res) => {
- 
-   const {name, email, password} = req.body
+  const { name, email, password } = req.body;
 
-   //check if user exists
-   const user = await User.findOne({email})
+  //check if user exists
+  const user = await User.findOne({ email });
 
-   if(user) {
-    res.status(422).json({errors: [ ]})
-   }
+  if (user) {
+    res.status(422).json({ errors: ["Po favor, utilize outro e-mail"] });
+    return;
+  }
 
+  //generate password hash
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(passo
+    
+
+  //create user
+  const newUser = await User.create({
+    name,
+    email,
+    passoword: passwordHash,
+  });
+
+  //if user was created sucessfully, return the token
+  if (!newUser) {
+    res
+      .status(422)
+      .json({ errors: ["Houve um erro, por favor tente mais tarde"] });
+    return;
+  }
+
+  res.status(201).json({
+    _id: newUser._id,
+    token: generateToken,
+  });
 };
 
 module.exports = {
